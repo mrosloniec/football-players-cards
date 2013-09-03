8be13b3a9f67ca86db8eb7824d7b3bd1d6d027a1
Michal Rosloniec <mrosloniec@gmail.com>
2013-09-03 22:07:45 +0200

    [2] Menu Główne
    Stworzyłem narazie tło (trawa) i przyciski w menu. Pod przyciskiem "nowa gra" jest podpięta akcja przejścia na inny ekran i dźwięk kliknięcia

--- src/com/football/players/cards/MainActivity.java
+++ src/com/football/players/cards/MainActivity.java
@@ -1,15 +1,25 @@
 package com.football.players.cards;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	public void startNewGame(View view) {
+		MediaPlayer buttonClick = MediaPlayer.create(this, R.raw.button_click);
+		buttonClick.start();
+
+		Intent intent = new Intent(this, NewGameActivity.class);
+		startActivity(intent);
+	}
 }
---
+++ src/com/football/players/cards/NewGameActivity.java
@@ -0,0 +1,21 @@
+package com.football.players.cards;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NewGameActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = getIntent();
+	}
+
+}
